0b93a98eb4609c9e524c881402f973ffb0b0aee3
rmilrad <ryan.milrad@yahoo.com>
2021-10-04 17:49:39 -0400

    updated database code

--- GetAthleteData.py
+++ GetAthleteData.py
@@ -3,7 +3,7 @@ import requests
 import json
 
 # Constants
-apiKey = ""
+apiKey = "3c343fadd6a5464897fdfcc5db72a8a3"
 HEADER = {'Ocp-Apim-Subscription-Key': apiKey }
 SDIO_URL = 'https://api.sportsdata.io/v3/nfl/stats/json/PlayerSeasonStats/2020'
 HOST = 'localhost'
@@ -28,17 +28,29 @@ def computePrice(athlete_data):
         computedAmericanFootballPrice = numerator / denominator
         return computedAmericanFootballPrice
 
-def run():
-        # Define the socket
-        # sock.connect((HOST, PORT)) # connect to socket
-        fd, addr = sock.accept()
-        # Loop the entire json
-        ListOfAthletes = getTheData()
-        print(addr)
-        for athlete in ListOfAthletes:
-                footballAthlete = "nfl,name=" + str(athlete["Name"]) + ",playerID=" + str(athlete["PlayerID"]) + " price=" + str(computePrice(athlete)) + "\n"
-                fd.sendall((footballAthlete).encode())
-        
-        fd.close() # close socket
+HOST = '146.59.10.118'
+PORT = 9009
+# For UDP, change socket.SOCK_STREAM to socket.SOCK_DGRAM
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-run()
+try:
+  sock.connect((HOST, PORT))
+  ListOfAthletes = getTheData()
+  for athlete in ListOfAthletes:
+        price = computePrice(athlete)
+        name = athlete['Name']
+        team = athlete['Team']
+        position = athlete['Position']
+        passingYards = athlete['PassingYards']
+        passingTouch = athlete['PassingTouchdowns']
+        reception = athlete['Receptions']
+        receiveYards = athlete['ReceivingYards']
+        receiveTouch = athlete['ReceivingTouchdowns']
+        rushingYards = athlete['RushingYards']
+        # rushingTouch = athlete['RushingTouch']
+
+        sock.sendall((f'nfl,name={name},team={team},position={position},passingYards={passingYards},passingTouchdowns={passingTouch},reception={reception},receiveYards={receiveYards},receiveTouch={receiveTouch},rushingYards={rushingYards} price={price}\n').encode())
+except socket.error as e:
+  print("Got error: %s" % (e))
+
+sock.close()
